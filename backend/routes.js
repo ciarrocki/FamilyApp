d7babe54e1ba00df14e05f2f9fcf113d7ac7f5ac
Daniel Ciarrocki <ciarrocki@gmail.com>
2021-02-13 20:10:23 -0500

    basic backend

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,18 @@
-var config = require('../db-config.js');
+
+var config = require("../db-config.js");
+
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://" + config.username + ":" 
-            + config.password + "@" + config.host 
-            + "/" + config.database + "?" + config.params;
+const uri = "mongodb+srv://" + config.username + ":" + config.password 
+            + "@" + config.host + "/" + config.dbname + "?" + config.params;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
+        
+client.connect(err => {
+  const collection = client.db("testdb1").collection("testcollection1");
+  // perform actions on the collection object
+  client.close();
+});
 
 client.connect(err => {
   const db = client.db('testdb1');
